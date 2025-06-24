6998a8d2e9b238fcd08d7231313dd4da4da89f27
krishmungase <mungasekrishna8@gmail.com>
2025-06-24 07:42:17 +0530

    fix: changes url

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173","https://true-doc-nine.vercel.app"],  
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"], 
